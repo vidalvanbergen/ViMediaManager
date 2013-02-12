bd487701c4d16eb3ced965e4e8979e40fdfb866a
Vidal van Bergen <vidal.vanbergen@gmail.com>
2013-02-13 00:53:24 +0100

    never mind the previous website.

--- vimediamanager/files/cocoa_latest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $hit_count = @file_get_contents('cocoa_count.txt');
-  $hit_count++;
-  @file_put_contents('cocoa_count.txt', $hit_count);
-
-  header('Location: http://mariusth.channelwood.org/vimediamanager/files/vimediamanager_cocoa.dmg'); // redirect to the real file to be downloaded
-?>
--- vimediamanager/files/manual_latest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $hit_count = @file_get_contents('intel_count.txt');
-  $hit_count++;
-  @file_put_contents('intel_count.txt', $hit_count);
-
-  header('Location: http://mariusth.channelwood.org/vimediamanager/files/user_manual.pdf'); // redirect to the real file to be downloaded
-?>
